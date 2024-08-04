01b8891a671336bb62dd62c3a1cb84d449f3fb8a
prakumar <praveen4892@gmail.com>
2024-08-04 12:15:02 +0530

    Added requested changes
     - Added all the annotations required for AuthorEntity class.

--- src/main/java/com/dev/sunny/model/Author.java
+++ src/main/java/com/dev/sunny/model/Author.java
@@ -1,16 +1,24 @@
 package com.dev.sunny.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
+
 @Getter
 @Setter
 @ToString
+@Entity
 public class Author {
 
-    int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String name;
     String email;
     List<String> publications;
